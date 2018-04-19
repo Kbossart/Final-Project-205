0d39aa5e35f33f10cba58fd60e5b7740310d32f5
Kbossart <35549614+Kbossart@users.noreply.github.com>
2018-04-18 20:26:45 -0600

    Add Feature Layer

--- js/my-app-code.js
+++ js/my-app-code.js
@@ -1,9 +1,25 @@
  
 document.addEventListener('DOMContentLoaded', function() {
-   var map = L.map('map').setView([37.75,-122.45], 12);
+   var map = L.map('map').setView([43.8041,-120.5542], 12);
     L.esri.basemapLayer("Topographic").addTo(map);
 L.esri.dynamicMapLayer({
     url: "https://mapservices.nps.gov/arcgis/rest/services/LandResourcesDivisionTractAndBoundaryService/MapServer",
     opacity: 1
   }).addTo(map);
 })
+
+map.on('mousemove', showLatLng);
+
+function showLatLng(e) {
+  document.getElementById("").innerText = (e.latlng.lat).toFixed(5) + " | " + (e.latlng.lng).toFixed(5);
+}
+document.getElementById("").onclick = function(){
+  map.setView([43.8041,-123.5542], 16);
+};
+var map = L.map('map')
+L.esri.featureLayerService({
+  url: https//gis.blm.gov/orarcgis/rest/services/Facility/BLM_OR_Recreation_Site/MapServer>opacity:1
+  }).addTo(map);
+
+map.on('click', onMapClick);
+
