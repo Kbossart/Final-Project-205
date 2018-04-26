e0095ce32ece9b1a0b6c9171b4b836d16efc7edd
Kbossart <35549614+Kbossart@users.noreply.github.com>
2018-04-25 18:59:19 -0600

    Added Feature layers
    Added feature layers and buttons

--- js/my-app-code.js
+++ js/my-app-code.js
@@ -1,25 +1,41 @@
  
 document.addEventListener('DOMContentLoaded', function() {
-   var map = L.map('map').setView([43.8041,-120.5542], 12);
+   var map = L.map('map').setView([43.8041,-120.5542], 8);
     L.esri.basemapLayer("Topographic").addTo(map);
 L.esri.dynamicMapLayer({
     url: "https://mapservices.nps.gov/arcgis/rest/services/LandResourcesDivisionTractAndBoundaryService/MapServer",
     opacity: 1
   }).addTo(map);
-})
-
+    L.esri.featureLayer({
+  url: "https://gis.blm.gov/orarcgis/rest/services/Facility/BLM_OR_Recreation_Site/MapServer/0"
+  }).addTo(map);
+    L.esri.featureLayer({
+  url: "https://gis.blm.gov/orarcgis/rest/services/Facility/BLM_OR_Recreation_Site/MapServer/1"
+  }).addTo(map);
+    L.esri.featureLayer({
+  url: "https://gis.blm.gov/orarcgis/rest/services/Facility/BLM_OR_Recreation_Site/MapServer/2"
+  }).addTo(map);
+   L.esri.featureLayer({
+  url: "https://gis.blm.gov/orarcgis/rest/services/Facility/BLM_OR_Recreation_Site/MapServer/3"
+  }).addTo(map); 
+   L.esri.featureLayer({
+  url: "https://gis.blm.gov/orarcgis/rest/services/Facility/BLM_OR_Recreation_Site/MapServer/4"
+  }).addTo(map);  
+    
 map.on('mousemove', showLatLng);
 
 function showLatLng(e) {
-  document.getElementById("").innerText = (e.latlng.lat).toFixed(5) + " | " + (e.latlng.lng).toFixed(5);
+  document.getElementById("latandlong").innerText = (e.latlng.lat).toFixed(5) + " | " + (e.latlng.lng).toFixed(5);
 }
-document.getElementById("").onclick = function(){
-  map.setView([43.8041,-123.5542], 16);
+    function setMapView(zoomLevel, latitude, longitude) {
+  map.setView([latitude, longitude], zoomLevel);
+}
+document.getElementById("goTo").onclick = goTo;
+ {
+  ;
 };
-var map = L.map('map')
-L.esri.featureLayerService({
-  url: https//gis.blm.gov/orarcgis/rest/services/Facility/BLM_OR_Recreation_Site/MapServer>opacity:1
-  }).addTo(map);
+    /*
+    map.setView([43.8041,-123.5542], 16)
+*/
 
-map.on('click', onMapClick);
-
+});
